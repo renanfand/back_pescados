27cb3de00971e7165f0142275670c90cf6c0c1a2
renanfand <renanfand@gmail.com>
2020-06-14 01:35:45 -0300

    Inclusão de Peixe em model

--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,15 @@ const dbConfig = require('../config/database');
 const Agricultor = require('../models/Agricultor');
 const TipoPeixe = require('../models/TipoPeixe');
 const Racao = require('../models/Racao');
+const Peixe = require('../models/Peixe');
 
 const connection = new Sequelize(dbConfig);
 
 Agricultor.init(connection);
 TipoPeixe.init(connection);
 Racao.init(connection);
+
+Racao.associate(connection);
+Peixe.associate(connection);
 
 module.exports = connection;
